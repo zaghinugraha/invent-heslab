38e947ad043f7097520a2e1c608998edebcab370
reihansyahfitra <reihansyahw@gmail.com>
2024-11-11 00:19:54 +0700

    added details on "low on stock" mechanism

--- resources/views/dashboard-admin-items.blade.php
+++ resources/views/dashboard-admin-items.blade.php
@@ -282,7 +282,7 @@
                 @endphp
                 background-color: {{ $color }};
             ">
-            {{ $product->quantity }}
+            {{ $product->quantity }}/{{ $product->quantity_alert }}
         </td>
         <td class="px-4 py-2 border relative group justify-center items-center">
           <svg class="w-7 h-7 m-auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
