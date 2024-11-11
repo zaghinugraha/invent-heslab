48fc71b17b20678af2d3dcc8f0ae24fac88086de
reihansyahfitra <reihansyahw@gmail.com>
2024-11-11 12:33:27 +0700

    add category to manage category

--- resources/views/dashboard-admin-items.blade.php
+++ resources/views/dashboard-admin-items.blade.php
@@ -223,7 +223,7 @@
 <div class="flex justify-between items-center mb-4">
     <!-- Left Side: Add Category Button -->
     <button @click="newCategory = true" class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">
-        Add Category
+        Manage Category
     </button>
 
     <!-- Right Side: Search Form and Add Item Button -->
