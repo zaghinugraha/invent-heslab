f37052c4a61633bb4940091a6b3f1db87a255524
reihansyahfitra <reihansyahw@gmail.com>
2024-11-27 02:15:12 +0700

    Refactor rent_items migration to improve foreign key constraints and ensure cascading deletes

--- database/migrations/2024_11_14_044420_rent-items.php
+++ database/migrations/2024_11_14_044420_rent-items.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,12 @@ return new class extends Migration
     {
         Schema::create('rent_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('rent_id')->constrained()->onDelete('cascade');
-            $table->foreignId('item_id')->constrained()->onDelete('cascade');
+            $table->foreignId('item_id')
+                ->constrained('products')
+                ->cascadeOnDelete();
+            $table->foreignId('rent_id')
+                ->constrained('rent')
+                ->cascadeOnDelete();
             $table->integer('quantity');
             $table->decimal('price_per_unit', 10, 2);
             $table->decimal('subtotal', 10, 2);
